23647cabcc285ec70397e09942f6994f08f2bfe6
Siu Cheng <herrcheng@gmail.com>
2022-05-19 22:57:31 +0200

    Initial commit

---
+++ src/test/java/com/ooka/uebung5/Uebung5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ooka.uebung5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uebung5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
