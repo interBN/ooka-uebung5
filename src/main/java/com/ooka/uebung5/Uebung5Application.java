6dfcd5448eb39824517ccfa1f6fad319d67da6a9
Siu Cheng <herrcheng@gmail.com>
2022-05-19 23:22:10 +0200

    add components

--- src/main/java/com/ooka/uebung5/Uebung5Application.java
+++ src/main/java/com/ooka/uebung5/Uebung5Application.java
@@ -1,13 +1,75 @@
 package com.ooka.uebung5;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Uebung5Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Uebung5Application.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Uebung5Application.class, args);
     }
 
+    public interface SaySomethingService {
+        String saySomething();
+    }
+
+    @RestController
+    public class SaySomethingController {
+        @Autowired
+        @Qualifier("sayHelloService")
+        SaySomethingService saySomethingService;
+
+        @GetMapping("/")
+        public String home() {
+            return saySomethingService.saySomething();
+        }
+    }
+
+    @Component
+    @Qualifier("sayHelloService")
+    public class SayHelloService implements SaySomethingService {
+        @Override
+        public String saySomething() {
+            return "Hello!";
+        }
+    }
+
+    @Configuration
+    public class SaySomethingConfiguration {
+        @Bean
+        @Primary
+        public SaySomethingConfigurableService saySomethingConfigurableService() {
+            SaySomethingConfigurableService saySomethingConfigurableService1 = new SaySomethingConfigurableService();
+            saySomethingConfigurableService1.setWhatToSay("Bye");
+            return saySomethingConfigurableService1;
+        }
+    }
+
+    public class SaySomethingConfigurableService implements SaySomethingService {
+
+        private String whatToSay = "";
+
+        @Override
+        public String saySomething() {
+            return whatToSay;
+        }
+
+        public String getWhatToSay() {
+            return whatToSay;
+        }
+
+        public void setWhatToSay(String whatToSay) {
+            this.whatToSay = whatToSay;
+        }
+    }
 }
