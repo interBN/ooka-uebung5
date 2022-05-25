3c273e0247c1c21eb54b4f7e1f36ea538fdb45bb
Siu Cheng <herrcheng@gmail.com>
2022-05-25 16:43:39 +0200

    add example for POST, PUT and DELETE

--- uebung5/src/main/java/com/ooka/test/TestApplication.java
+++ uebung5/src/main/java/com/ooka/test/TestApplication.java
@@ -9,6 +9,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"com.ooka.*"})
 @EntityScan("com.ooka.*")
@@ -28,19 +32,84 @@ public class TestApplication {
 //        CityRepository cityRepository = applicationContext.getBean(CityRepository.class);
 //        cityRepository.save(berlin);
 //        cityRepository.save(cologne);
+
+        int counter = 0;
+
         while (applicationContext.isActive()) {
+
             try {
                 Thread.sleep(1000);
-                RestTemplate restTemplate = new RestTemplate();
-                ResponseEntity<NumberEntity> numberEntity = restTemplate.getForEntity("http://localhost:8090/numbers/1", NumberEntity.class);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            RestTemplate restTemplate = new RestTemplate();
+
+            if (counter == 0) {
+                /*
+                  POST random number
+                 */
+                int i = new Random().nextInt();
+                NumberEntity number = new NumberEntity();
+                number.setValue(i);
+                restTemplate.postForObject("http://localhost:8090/numbers", number, NumberEntity.class);
+                System.out.println("POST => value: " + number.getValue());
+            } else if (counter == 1) {
+                long id = getLastId(restTemplate);
+                ResponseEntity<NumberEntity> numberEntity = restTemplate.getForEntity("http://localhost:8090/numbers/" + id, NumberEntity.class);
                 if (numberEntity.hasBody()) {
-                    NumberEntity body = numberEntity.getBody();
-                    System.out.println(body.getValue());
+                    /*
+                    GET last entry and change value
+                     */
+                    NumberEntity entity = numberEntity.getBody();
+                    assert entity != null;
+                    entity.setValue(-1);
+                    /*
+                     PUT
+                    */
+                    Map<String, String> params = new HashMap<>();
+                    params.put("id", String.valueOf(id));
+                    restTemplate.put("http://localhost:8090/numbers/" + id, entity, params);
+                    System.out.println("PUT => id: " + id + ", value: " + entity.getValue());
                 }
-            } catch (Exception e) {
-                break;
+            } else if (counter == 2) {
+                long id = getLastId(restTemplate);
+               /*
+                 DELETE
+                */
+                Map<String, String> params = new HashMap<>();
+                params.put("id", String.valueOf(id));
+                restTemplate.delete("http://localhost:8090/numbers/" + id, params);
+                System.out.println("DELETE => id: " + id);
             }
 
+            /*
+              GET and print id 0
+             */
+            ResponseEntity<NumberEntity> numberEntity = restTemplate.getForEntity("http://localhost:8090/numbers/1", NumberEntity.class);
+            if (numberEntity.hasBody()) {
+                NumberEntity body = numberEntity.getBody();
+                System.out.println(body.getValue());
+            }
+
+            counter++;
+            counter %= 3;
         }
+    }
+
+    private static long getLastId(RestTemplate restTemplate) {
+        ResponseEntity<NumberEntity[]> allEntries = restTemplate.getForEntity("http://localhost:8090/numbers/getAll", NumberEntity[].class);
+        long id = -1L;
+        if (allEntries.hasBody()) {
+            NumberEntity[] body = allEntries.getBody();
+            assert body != null;
+            Long max = 0L;
+            for (NumberEntity e : body) {
+                if (e.getId() > max) {
+                    max = e.getId();
+                }
+            }
+            id = max;
+        }
+        return id;
     }
 }
