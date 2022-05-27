52defea6c00ccfbba4bce5e4e599c012a1f2a30f
Siu Cheng <herrcheng@gmail.com>
2022-05-27 22:49:08 +0200

    :memo: update ports and banner

--- analysis-control/src/main/java/com/ooka/analysis/AnalysisController.java
+++ analysis-control/src/main/java/com/ooka/analysis/AnalysisController.java
@@ -1,23 +1,20 @@
 package com.ooka.analysis;
 
 import com.ooka.analysis.product.Product;
-import com.ooka.analysis.product.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping("")
 public class AnalysisController {
-    private State state = State.IDLE;
+    private final State state = State.IDLE;
 
     @GetMapping(value = "/state", produces = "application/json")
     public ResponseEntity<State> getState() {
@@ -26,11 +23,11 @@ public class AnalysisController {
 
 
     @PutMapping("/run")
-    public void runController(){
+    public void runController() {
         new Thread(this::run).start();
     }
 
-    private void run(){
+    private void run() {
 
         long start = new Date().getTime();
         RestTemplate restTemplate = new RestTemplate();
@@ -48,23 +45,23 @@ public class AnalysisController {
         product.setPowerTransmission("I");
         product.setGearbox("J");
         product.setMountingSystem("K");
-        restTemplate.postForObject("http://localhost:8070/products", product, Product.class);
+        restTemplate.postForObject("http://localhost:8072/products", product, Product.class);
 
         System.out.println("Starting AlgorithmA");
         int resultA = -1;
         try {
-            resultA = getResult("http://localhost:8071/algorithmA", product, restTemplate);
+            resultA = getResult("http://localhost:8070/algorithmA", product, restTemplate);
             System.out.println("resultA = " + resultA);
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Error while performing AlgorithmA");
         }
 
         System.out.println("Starting AlgorithmB");
         int resultB = -1;
         try {
-            resultB = getResult("http://localhost:8073/algorithmB", product, restTemplate);
+            resultB = getResult("http://localhost:8071/algorithmB", product, restTemplate);
             System.out.println("resultB = " + resultB);
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Error while performing AlgorithmB");
         }
 
@@ -75,8 +72,9 @@ public class AnalysisController {
         System.out.println("Duration = " + ((end - start) / 1000) + " sec");
 
         product.setResult(result);
-        restTemplate.postForObject("http://localhost:8070/products", product, Product.class);
+        restTemplate.postForObject("http://localhost:8072/products", product, Product.class);
     }
+
     private int getResult(String baseUrl, Product product, RestTemplate restTemplate) throws Exception {
         return getResult(baseUrl, product, restTemplate, 0);
     }
