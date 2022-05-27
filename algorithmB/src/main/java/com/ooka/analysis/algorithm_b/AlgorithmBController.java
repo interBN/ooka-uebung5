945a7a334c17874f0ff7ed1c613a577eea897242
Thorsten Laufenberg <thorsten@laufenberg.eu>
2022-05-27 14:03:32 +0200

    Second Algorithm

---
+++ algorithmB/src/main/java/com/ooka/analysis/algorithm_b/AlgorithmBController.java
@@ -0,0 +1,88 @@
+package com.ooka.analysis.algorithm_b;
+
+import com.ooka.analysis.State;
+import com.ooka.analysis.product.Product;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/algorithmB")
+public class AlgorithmBController {
+
+    @Autowired
+    AlgorithmBRepository algorithmBRepository;
+
+    private State state = State.IDLE;
+
+    @PutMapping("")
+    public void runAlgorithm(@RequestBody Product product) {
+        new Thread(() -> run(product)).start();
+    }
+
+    private void run(Product product) {
+        if (state != State.RUNNING) {
+            state = State.RUNNING;
+            int analysisTime = 7500;
+//                    ThreadLocalRandom.current().nextInt(100, 10000);
+            AlgorithmBEntity alg = new AlgorithmBEntity();
+            try {
+                Thread.sleep(analysisTime);
+                if (Math.random() < 0.1) {
+                    throw new Exception();
+                }
+                state = State.SUCCEEDED;
+                alg.setLog("Analysis of " + product.getAuxiliaryPTO() + "Succeeded. Analysis time: " + analysisTime + "ms");
+            } catch (Exception e) {
+                state = State.FAILED;
+                alg.setLog("Analysis failed.");
+            }
+            algorithmBRepository.save(alg);
+        }
+    }
+
+    @GetMapping(value = "result", produces = "application/json")
+    public ResponseEntity<Object> getResult() {
+        JSONObject entity = new JSONObject();
+        HttpStatus status;
+        entity.put("state", state);
+        if (state == State.SUCCEEDED) {
+            entity.put("result", 15635);
+            status = HttpStatus.OK;
+        } else {
+            status = HttpStatus.NO_CONTENT;
+        }
+        return new ResponseEntity<>(entity.toString(), status);
+    }
+
+    @GetMapping("/{algorithmBId}")
+    public String readLog(@PathVariable Long algorithmBId) {
+        Optional<AlgorithmBEntity> algorithmB = algorithmBRepository.findById(algorithmBId);
+        if (algorithmB.isPresent()) {
+            return algorithmB.get().getLog();
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Log with " + algorithmBId + " not found");
+    }
+
+    @GetMapping(value = "/state", produces = "application/json")
+    public ResponseEntity<Object> getState() {
+        JSONObject entity = new JSONObject();
+        entity.put("state", state);
+        return new ResponseEntity<>(entity.toString(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{algorithmBId}")
+    public void deleteLog(@PathVariable Long algorithmBId) {
+        Optional<AlgorithmBEntity> algorithmB = algorithmBRepository.findById(algorithmBId);
+        if (algorithmB.isPresent()) {
+            algorithmBRepository.delete(algorithmB.get());
+            return;
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Log with " + algorithmBId + " not found");
+    }
+}
---
+++ algorithmB/src/main/java/com/ooka/analysis/algorithm_b/AlgorithmBRepository.java
@@ -0,0 +1,8 @@
+package com.ooka.analysis.algorithm_b;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlgorithmBRepository extends CrudRepository<AlgorithmBEntity, Long> {
+}
