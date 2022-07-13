fd33736aeb68a566ae34fb374ecb8460b51844b2
Thorsten Laufenberg <thorsten@laufenberg.eu>
2022-07-13 17:29:50 +0200

    New Algorithm copy

---
+++ algorithmC/src/main/java/com/ooka/analysis/algorithm_c/AlgorithmCRepository.java
@@ -0,0 +1,8 @@
+package com.ooka.analysis.algorithm_c;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlgorithmCRepository extends CrudRepository<AlgorithmCEntity, Long> {
+}
